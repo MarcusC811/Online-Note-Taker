6689aff3e946e809fdf9a6f356a525554ada2e53
OVOMARK <mcncstatefan@icloud.com>
2022-11-16 12:06:51 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// const api = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// app.use('/api', api);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
