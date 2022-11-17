c938ff35dfe92ea5810547c9648d1888087a3b93
OVOMARK <mcncstatefan@icloud.com>
2022-11-17 14:57:48 -0500

    updated routing to handling writing, rendering and deletion

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,10 @@
 const api = require('express').Router();
+const fs = require('fs');
+const path = require('path');
 
 api.post('/notes', (req, res) => {
     console.info(`${req.method} request received to add a review`);
+    let prevNotes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json')));
 
     const { title, text } = req.body;
 
@@ -9,8 +12,27 @@ api.post('/notes', (req, res) => {
         title,
         text
     }
-
-    console.log(newNotes);
+    prevNotes.push(newNotes);
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(prevNotes));
+    // Re-render the notes list
+    res.json(prevNotes);
 })
+
+api.get('/notes', (req, res) => {
+    let prevNotes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json')));
+    // Renders db file
+    res.json(prevNotes);
+})
+
+ api.delete('/notes/:id', (req, res) => {
+    let prevNotes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json')));
+    let keepNotes = prevNotes.filter(item => {
+        return item.id !== req.params.id
+    })
+
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(keepNotes));
+    res.json(keepNotes);
+
+ })
 
 module.exports = api;
